dc9323a6583d74b9b7e667ceabffc98ab997de09
CB-RyanMcCormick <122290337+CB-RyanMcCormick@users.noreply.github.com>
2023-05-30 13:14:14 -0500

    Add proguard rules (#49)
    * Add proguard rules
    
    * Update docs.
    
    * Include rules in AAR
    
    * Update docs

--- AppLovinAdapter/build.gradle.kts
+++ AppLovinAdapter/build.gradle.kts
@@ -35,8 +35,10 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.11.8.2.1"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.11.8.2.2"
         buildConfigField("String", "CHARTBOOST_MEDIATION_APPLOVIN_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
+
+        consumerProguardFiles("proguard-rules.pro")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
