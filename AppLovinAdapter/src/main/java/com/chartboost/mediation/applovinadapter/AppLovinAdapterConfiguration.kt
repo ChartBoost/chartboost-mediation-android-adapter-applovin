637a04d15566b28ec957afb3c387129619e61f9d
vu <vu.chau@chartboost.com>
2024-05-02 17:47:52 -0400

    AdapterConfiguration

---
+++ AppLovinAdapter/src/main/java/com/chartboost/mediation/applovinadapter/AppLovinAdapterConfiguration.kt
@@ -0,0 +1,115 @@
+package com.chartboost.mediation.applovinadapter
+
+import android.content.Context
+import android.provider.Settings
+import com.applovin.sdk.AppLovinSdk
+import com.chartboost.chartboostmediationsdk.domain.PartnerAdapterConfiguration
+import com.chartboost.chartboostmediationsdk.utils.PartnerLogController
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+object AppLovinAdapterConfiguration : PartnerAdapterConfiguration {
+    /**
+     * The partner name for internal uses.
+     */
+    override val partnerId = "applovin"
+
+    /**
+     * The partner name for external uses.
+     */
+    override val partnerDisplayName = "AppLovin"
+
+    /**
+     * The partner SDK version.
+     */
+    override val partnerSdkVersion: String = AppLovinSdk.VERSION
+
+    /**
+     * The partner adapter version.
+     *
+     * You may version the adapter using any preferred convention, but it is recommended to apply the
+     * following format if the adapter will be published by Chartboost Mediation:
+     *
+     * Chartboost Mediation.Partner.Adapter
+     *
+     * "Chartboost Mediation" represents the Chartboost Mediation SDK’s major version that is compatible with this adapter. This must be 1 digit.
+     * "Partner" represents the partner SDK’s major.minor.patch.x (where x is optional) version that is compatible with this adapter. This can be 3-4 digits.
+     * "Adapter" represents this adapter’s version (starting with 0), which resets to 0 when the partner SDK’s version changes. This must be 1 digit.
+     */
+    override val adapterVersion = BuildConfig.CHARTBOOST_MEDIATION_APPLOVIN_ADAPTER_VERSION
+
+    /**
+     * Enable/disable AppLovin's test mode. Remember to set this to false in production.
+     *
+     * @param context The current [Context].
+     * @param enabled True to enable test mode, false otherwise.
+     */
+    suspend fun setTestMode(
+            context: Context,
+            enabled: Boolean,
+    ) {
+        val adIds =
+                withContext(Dispatchers.IO) {
+                    try {
+                        AdvertisingIdClient.getAdvertisingIdInfo(context).id
+                    } catch (e: Exception) {
+                        context.contentResolver.let { resolver ->
+                            Settings.Secure.getString(resolver, "advertising_id")
+                        }
+                    }?.takeIf { enabled }?.let { listOf(it) } ?: emptyList()
+                }
+
+        updateSdkSetting("test mode", enabled) {
+            settings.testDeviceAdvertisingIds = adIds
+        }
+    }
+
+    /**
+     * Enable/disable AppLovin's mute setting.
+     *
+     * @param muted True to mute video creatives, false otherwise.
+     */
+    fun setMuted(muted: Boolean) {
+        updateSdkSetting("mute setting", muted) {
+            settings.isMuted = muted
+        }
+    }
+
+    /**
+     * Enable/disable AppLovin's verbose logging.
+     *
+     * @param enabled True to enable verbose logging, false otherwise.
+     */
+    fun setVerboseLogging(enabled: Boolean) {
+        updateSdkSetting("verbose logging", enabled) {
+            settings.setVerboseLogging(enabled)
+        }
+    }
+
+    /**
+     * Generic function to update AppLovin SDK settings and log the action.
+     *
+     * @param settingName The name of the setting being modified.
+     * @param enabled Whether the setting is enabled or not.
+     * @param action The action to perform on the AppLovin SDK.
+     */
+    private fun updateSdkSetting(
+            settingName: String,
+            enabled: Boolean,
+            action: AppLovinSdk.() -> Unit,
+    ) {
+        AppLovinAdapter.appLovinSdk?.let { sdk ->
+            sdk.action()
+
+            val status = if (enabled) "enabled" else "disabled"
+            PartnerLogController.log(
+                    PartnerLogController.PartnerAdapterEvents.CUSTOM,
+                    "AppLovin $settingName is $status.",
+            )
+        } ?: PartnerLogController.log(
+                PartnerLogController.PartnerAdapterEvents.CUSTOM,
+                "Unable to set $settingName. AppLovin SDK instance is null.",
+        )
+    }
+}
