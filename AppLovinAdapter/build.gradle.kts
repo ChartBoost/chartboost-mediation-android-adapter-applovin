03bef59fe41e8dd87cee756d30511c97a234eedc
chauduyphanvu <vu.chau@chartboost.com>
2024-06-04 16:10:25 -0400

    API 34 (#80)

--- AppLovinAdapter/build.gradle.kts
+++ AppLovinAdapter/build.gradle.kts
@@ -36,11 +36,11 @@ repositories {
 
 android {
     namespace = "com.chartboost.mediation.applovinadapter"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
         android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.12.5.0.0"
         buildConfigField("String", "CHARTBOOST_MEDIATION_APPLOVIN_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
