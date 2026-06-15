511e8ee09401d0575da542b7bc89df7d742ea29e
Helium Service <103450738+helium-service@users.noreply.github.com>
2026-06-15 09:08:03 -0700

    5.13.6.3.0 (#111)

--- AppLovinAdapter/build.gradle.kts
+++ AppLovinAdapter/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         minSdk = 21
         targetSdk = 34
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.13.6.2.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.13.6.3.0"
         buildConfigField("String", "CHARTBOOST_MEDIATION_APPLOVIN_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         consumerProguardFiles("proguard-rules.pro")
@@ -99,7 +99,7 @@ dependencies {
     "candidateImplementation"("com.chartboost:chartboost-mediation-sdk:5.0.0")
 
     // Partner SDK
-    implementation("com.applovin:applovin-sdk:13.6.2")
+    implementation("com.applovin:applovin-sdk:13.6.3")
 
     // Partner SDK Dependencies
     implementation("androidx.appcompat:appcompat:1.5.1")
